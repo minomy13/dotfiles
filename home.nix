6622dd538e5731b9948e0bdc6cc7637e94f1f60f
minomy13 <mail@minomy13.com>
2026-01-04 02:26:09 +0100

    made alacritty transparent

--- home.nix
+++ home.nix
@@ -20,7 +20,10 @@
       font = {
         normal = { family = "JetBrainsMonoNF"; style = "Regular"; };
       };
-      window.padding = { x = 15; y = 8; };
+      window = {
+        padding = { x = 15; y = 8; };
+	opacity = 0.8;
+      };
     };
   };
 
