bb5f405d4a19280dae649b3ab2023214ba4e6410
minomy13 <mail@minomy13.com>
2026-02-26 01:28:13 +0100

    added volume to waybar, added pavucontrol, reformatting

--- waybar/default.nix
+++ waybar/default.nix
@@ -1,32 +1,53 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ jq ];
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    jq
+    pavucontrol
+  ];
 
   programs.waybar = {
     enable = true;
     settings.main = {
       spacing = 4;
 
-      modules-left = [ "custom/nixos" "hyprland/workspaces" ];
+      modules-left = [
+        "custom/nixos"
+        "hyprland/workspaces"
+      ];
       modules-center = [ "hyprland/window" ];
-      modules-right = [ "network" "cpu" "memory" "custom/temp" "custom/water-temp" "clock" ];
+      modules-right = [
+        "network"
+        "pulseaudio"
+        "cpu"
+        "memory"
+        "custom/temp"
+        "custom/water-temp"
+        "clock"
+      ];
 
       "custom/nixos" = {
         format = "";
-	tooltip = false;
+        tooltip = false;
       };
 
       "hyprland/workspaces" = {
         disable-scroll = true;
-	all-outputs = true;
-	warp-on-scroll = false;
-	format = "{name}";
+        all-outputs = true;
+        warp-on-scroll = false;
+        format = "{name}";
       };
 
       network = {
         format-ethernet = "󰌗  {ipaddr}/{cidr}";
-	format-wifi = "   {essid} ({signalStrength}%)";
-	format-disconnected = "Disconnected ⚠";
-	tooltip = false;
+        format-wifi = "   {essid} ({signalStrength}%)";
+        format-disconnected = "Disconnected ⚠";
+        tooltip = false;
+      };
+
+      pulseaudio = {
+        format = "   {volume}%";
+        format-muted = " ";
+        on-click = "pavucontrol";
       };
 
       cpu = {
@@ -40,15 +61,15 @@
       "custom/temp" = {
         exec = ./temperature.sh;
         interval = 10;
-	format = " {text}";
-	tooltip = false;
+        format = " {text}";
+        tooltip = false;
       };
 
       "custom/water-temp" = {
         exec = ./water-temperature.sh;
-	interval = 30;
-	format = "󰏈  {text}";
-	tooltip = false;
+        interval = 30;
+        format = "󰏈  {text}";
+        tooltip = false;
       };
 
       clock = {
