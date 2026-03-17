24b7d9b5a63ee9ccaea6cdec3863bed5a1c3429c
minomy13 <mail@minomy13.com>
2026-03-17 01:02:47 +0100

    made librewolf persistent across reboots and added 1password extension

--- modules/gui/librewolf.nix
+++ modules/gui/librewolf.nix
@@ -2,5 +2,25 @@
 {
   hm.programs.librewolf = {
     enable = true;
+
+    settings = {
+      "webgl.disabled" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "network.cookie.lifetimePolicy" = 0;
+    };
+
+    policies = {
+      BlockAboutConfig = true;
+      ExtensionSettings = {
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          default_area = "menupanel";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+      };
+    };
   };
 }
