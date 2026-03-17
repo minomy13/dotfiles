bf1e867d4ddd802f84f97ffbab3e8447e143e02d
minomy13 <mail@minomy13.com>
2026-03-17 01:02:09 +0100

    added librewolf to allowed browsers for 1password extension

--- modules/gui/1password.nix
+++ modules/gui/1password.nix
@@ -4,4 +4,13 @@
     _1password.enable = true;
     _1password-gui.enable = true;
   };
+
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        librewolf
+      '';
+      mode = "0755";
+    };
+  };
 }
