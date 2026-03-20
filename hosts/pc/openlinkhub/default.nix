5c3f9fcfd2da5319c30cd278c5fc8d7323785cbc
minomy13 <mail@minomy13.com>
2026-03-20 20:22:24 +0100

    updated path of openlinkhub config

--- hosts/pc/openlinkhub/default.nix
+++ hosts/pc/openlinkhub/default.nix
@@ -1,14 +1,20 @@
+{ ... }:
 {
   virtualisation.oci-containers = {
     backend = "docker";
     containers = {
       openlinkhub = {
-	image = "ghcr.io/jurkovic-nikola/openlinkhub:0.7.4";
+        image = "ghcr.io/jurkovic-nikola/openlinkhub:0.7.4";
         autoStart = true;
-	privileged = true;
-	volumes = [ "/home/adrian/dotfiles/openlinkhub/config.json:/opt/OpenLinkHub/config.json" "icue:/opt/OpenLinkHub/database" ];
-	networks = [ "host" ];
+        privileged = true;
+        volumes = [
+          "/home/adrian/.config/openlinkhub.json:/opt/OpenLinkHub/config.json"
+          "icue:/opt/OpenLinkHub/database"
+        ];
+        networks = [ "host" ];
       };
     };
   };
+
+  hm.home.file.".config/openlinkhub.json".source = ./config.json;
 }
