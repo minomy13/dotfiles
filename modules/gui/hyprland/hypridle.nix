de1dd34965a8ed6518ec1bf892bdb68c15528070
minomy13 <mail@minomy13.com>
2026-04-09 13:05:50 +0200

    set up hypridle

---
+++ modules/gui/hyprland/hypridle.nix
@@ -0,0 +1,35 @@
+{ ... }:
+{
+  hm.services.hypridle = {
+    enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        {
+          timeout = 120;
+          on-timeout = "brightnessctl -c backlight -s set 10";
+          on-resume = "brightnessctl -c backlight -r";
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
